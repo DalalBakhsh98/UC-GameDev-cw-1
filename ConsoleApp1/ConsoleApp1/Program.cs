7e027ff62a92a5b5d97015879167a23ab5719d4c
DalalBakhsh98 <69141269+DalalBakhsh98@users.noreply.github.com>
2022-06-22 19:41:14 +0300

    My code for the villain and hero story

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hero information
+            string heroName = "Vincent";
+            int HeroH = 190;
+            int HeroAge = 35;
+            string HeroSuperP = "Elastic";
+
+            //Viilain information
+            string VillainName = "Abdu";
+            int VillH = 150;
+            int VillAge = 22;
+            string VillSuperP = "Annoying";
+
+            int agedifference = HeroAge - VillAge;
+
+            Console.WriteLine("Hello My name is "+heroName+ " and my height is "+HeroH+ "and my age is "+HeroAge+" and my super power is"+HeroSuperP+"");
+            Console.WriteLine("Hello My name is " + VillainName + " and my height is " + VillH + "and my age is " + VillAge + " and my super power is " + VillSuperP+"");
+            Console.WriteLine("The age difference is "+ agedifference+" ");
+
+
+            HeroAge = HeroAge + 5;
+            VillSuperP = "None";
+
+
+
+            Console.WriteLine("Hello My name is " + heroName + " and my height is " + HeroH + "and my age is " + HeroAge + " and my super power is" + HeroSuperP + "");
+            Console.WriteLine("Hello My name is " + VillainName + " and my height is " + VillH + "and my age is " + VillAge + " and my super power is " + VillSuperP + "");
+            Console.WriteLine("The age difference is " + agedifference + "");
+
+
+        }
+    }
+}
